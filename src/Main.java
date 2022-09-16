9974c857d8b348fc80e441b538f80acaa78e3ab3
Lookie4Fun <94829223+Lookie4Fun@users.noreply.github.com>
2022-09-16 13:20:40 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,41 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String [] args){
-        String jdbURL = "jdbc:postgresql://localhost:5432/ovchip";
-        String username = "postgres";
-        String password = "1234";
+    private static Connection connection = null;
 
-        try {
-            Connection connection = DriverManager.getConnection(jdbURL,username,password);
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("select * from reiziger");
+    public static void main(String [] args) {
+        if (connection == null) {
+            String jdbURL = "jdbc:postgresql://localhost:5432/ovchip";
+            String username = "postgres";
+            String password = "1234";
 
-            while (result.next()){
-                String tussenvoegsel = result.getString("tussenvoegsel");
-                if(tussenvoegsel == null){
-                    tussenvoegsel="";
-                }
-                System.out.println(result.getString("reiziger_id")+": "+result.getString("voorletters")+". "+tussenvoegsel+result.getString("achternaam")+" ("+result.getString("geboortedatum")+")");
+            try {
+                connection = DriverManager.getConnection(jdbURL, username, password);
+            } catch (SQLException e) {
+                System.out.println("Error");
+                e.printStackTrace();
             }
 
 
-        } catch (SQLException e) {
-            System.out.println("Error");
-            e.printStackTrace();
-        }
+            try {
 
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery("select * from reiziger");
+
+                while (result.next()) {
+                    String tussenvoegsel = result.getString("tussenvoegsel");
+                    if (tussenvoegsel == null) {
+                        tussenvoegsel = "";
+                    }
+                    System.out.println(result.getString("reiziger_id") + ": " + result.getString("voorletters") + ". " + tussenvoegsel + result.getString("achternaam") + " (" + result.getString("geboortedatum") + ")");
+                }
+
+
+            } catch (SQLException e) {
+                System.out.println("Error");
+                e.printStackTrace();
+            }
+
+        }
     }
 }
