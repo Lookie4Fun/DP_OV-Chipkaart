4ae28b851d633ff571e5dec88f5750d424ad56c0
Nick <nick.deheij@student.hu.nl>
2022-10-14 13:32:09 +0200

    p5

--- src/OVChipkaartDAOsql.java
+++ src/OVChipkaartDAOsql.java
@@ -11,7 +11,6 @@ public class OVChipkaartDAOsql implements OVChipkaartDAO{
         this.conn = conn;
     }
 
-
     @Override
     public List<OVChipkaart> findAll() throws SQLException {
         List<OVChipkaart> kaarten = new ArrayList<>();
